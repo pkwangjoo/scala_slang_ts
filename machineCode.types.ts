e7042fa8dd693a1f23fd4e07cbdd88b7d74c02d8
Ivan <77185900+wpinrui@users.noreply.github.com>
2024-03-31 16:47:01 +0800

    Create machineCode.types.ts

---
+++ machineCode.types.ts
@@ -0,0 +1,67 @@
+type LDC = {
+  tag: "LDC",
+  val: number
+}
+
+type LD = {
+  tag: "LD",
+  sym: string
+}
+
+type UNOP = {
+  tag: "UNOP",
+  sym: string
+}
+
+type BINOP = {
+  tag: "BINOP",
+  sym: string
+}
+
+type JOF = {
+  tag: "JOF",
+}
+
+type GOTO = {
+  tag: "GOTO",
+}
+
+type CALL = {
+  tag: "CALL",
+  arity: number
+}
+
+type ASSIGN = {
+  tag: "ASSIGN",
+  sym: string
+}
+
+type LDF = {
+  tag: "LDF",
+  prms: string[],
+  addr: number
+}
+
+type RESET = {
+  tag: "RESET"
+}
+
+type ENTER_SCOPE = {
+  tag: "ENTER_SCOPE"
+  syms: string[]
+}
+
+type EXIT_SCOPE = {
+  tag: "EXIT_SCOPE"
+}
+
+type TAIL_CALL = {
+  tag: "TAIL_CALL"
+  arity: number
+}
+
+type DONE = {
+  tag: "DONE"
+}
+
+type Instruction = LDC | LD | UNOP | BINOP | JOF | GOTO;
