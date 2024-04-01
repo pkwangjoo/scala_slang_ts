d9f544eea46d1c00a7d287fb42169e6563d2f58e
kwangjoo <pkj5257@gmail.com>
2024-04-01 11:18:33 +0800

    Remove tag in favour of types

--- src/interpreter/machineCode.types.ts
+++ src/interpreter/machineCode.types.ts
@@ -1,20 +1,16 @@
 type LDC = {
-  tag: "LDC",
   val: number
 }
 
 type LD = {
-  tag: "LD",
   sym: string
 }
 
 type UNOP = {
-  tag: "UNOP",
   sym: string
 }
 
 type BINOP = {
-  tag: "BINOP",
   sym: string
 }
 
@@ -27,41 +23,33 @@ type GOTO = {
 }
 
 type CALL = {
-  tag: "CALL",
   arity: number
 }
 
 type ASSIGN = {
-  tag: "ASSIGN",
   sym: string
 }
 
 type LDF = {
-  tag: "LDF",
   prms: string[],
   addr: number
 }
 
 type RESET = {
-  tag: "RESET"
 }
 
 type ENTER_SCOPE = {
-  tag: "ENTER_SCOPE"
   syms: string[]
 }
 
 type EXIT_SCOPE = {
-  tag: "EXIT_SCOPE"
 }
 
 type TAIL_CALL = {
-  tag: "TAIL_CALL"
   arity: number
 }
 
 type DONE = {
-  tag: "DONE"
 }
 
 type Instruction = LDC | LD | UNOP | BINOP | JOF | GOTO | CALL | ASSIGN | LDF | RESET | ENTER_SCOPE | EXIT_SCOPE | TAIL_CALL | DONE
