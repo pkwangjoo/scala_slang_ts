c7a854f3e232c25ce11c3f82fd2e7ee7055d8bfd
kwangjoo <pkj5257@gmail.com>
2024-04-01 13:33:13 +0800

    Add kind

--- src/ast.types.ts
+++ src/ast.types.ts
@@ -1,50 +1,63 @@
 
 type Name = {
+    kind: "name";
     sym: string;
 }
 
 type IntLit = {
+    kind: "intlit"
     val : number
 }
 
 type BinaryOperation = {
+    kind: "binop"
     operand1 : Expression
     operand2 : Expression
     operator : string
 }
 
 type LambdaExpr = {
+    kind: "lambda"
     params : string[]
     body : BlockStat | Expression
 }
 
+
 type CondExpr = {
+    kind: "cond"
     pred : Expression // must be logical expression
     conseq: Expression
     alt: Expression
 }
 
+
 type AssignmentStat = {
+    kind: "assign"
     name : string
     expr: Expression
 }
 
 type FunctionDefStat = {
+    kind: "fundef"
     name: string
     lambda: LambdaExpr
 }
 
+
 type BlockStat = {
+    kind: "block"
     stmts: Statement[] 
 }
 
 type IfStat = {
+    kind: "ifstat"
     pred: Expression
     conseq: BlockStat
     alt: BlockStat | IfStat
 }
 
 type Sequence = {
+    kind: "seq"
     stmts: Statement[];
 }
 
--- src/parser/ScalaSlangVisitorInstance.ts
+++ src/parser/ScalaSlangVisitorInstance.ts
@@ -21,17 +21,19 @@ export class ScalaSlangVisitorInstance
     visitProg(ctx: ProgContext) : Sequence {
         
         return {
+            kind : "seq",
             stmts: ctx.children!.map(s => this.visit(s) as Statement)
         }
     }
 
     visitAssignstat(ctx: AssignstatContext) : AssignmentStat {
-        const name = ctx._name.text;
+        const name = ctx._name.text!;
         const expr = ctx.expr();
         return {
             name,
-            expr: this.visit(expr)
-        } as AssignmentStat
+            kind : "assign",
+            expr: this.visit(expr) as Expression
+        } 
     }
 
     visitFundefstat(ctx: FundefstatContext) : FunctionDefStat {
@@ -39,11 +41,13 @@ export class ScalaSlangVisitorInstance
         const params = this.visit(ctx.names());
         const body = this.visit(ctx.block()) as BlockStat;
         const lambdaExpr = {
+            kind: "lambda",
             params,
             body
         } as LambdaExpr
 
         return {
+            kind: "fundef",
             name, 
             lambda: lambdaExpr
         } as FunctionDefStat
@@ -67,6 +71,7 @@ export class ScalaSlangVisitorInstance
     visitBlock(ctx : BlockContext) : BlockStat {
         const stmts = ctx.getRuleContexts(StatContext).map(r => this.visit(r) as Statement);
         return {
+            kind: "block",
             stmts
         }
     }
@@ -77,6 +82,7 @@ export class ScalaSlangVisitorInstance
         const conseq = this.visit(ctx.block(0)) as BlockStat
         if (ctx.ifstat()) {
             return {
+                kind: "ifstat",
                 pred,
                 conseq,
                 alt: this.visit(ctx.ifstat()!) as IfStat
@@ -85,10 +91,11 @@ export class ScalaSlangVisitorInstance
 
         const alt = this.visit(ctx.block(1)) as BlockStat;
         return {
+            kind: "ifstat",
             pred,
             conseq, 
             alt 
-        } as IfStat
+        } 
     }
 
     visitBinopexpr(ctx: BinopexprContext): BinaryOperation {
@@ -96,22 +103,26 @@ export class ScalaSlangVisitorInstance
         const scnd = ctx.expr(1);
         const operator = ctx._op.text!;
 
-        const res = {
+        return {
+            kind: "binop",
             operand1: this.visit(fst) as Expression,
             operand2: this.visit(scnd) as Expression,
             operator
         }
 
-        return res;
     }
 
     visitIntlit(ctx: IntlitContext) : IntLit {
         return {
+            kind: "intlit",
             val: parseInt(ctx._val.text!),
         } 
     }
     visitName(ctx: NameContext) : Name {
-        return {sym: ctx._name.text!}
+        return {
+            kind: "name",
+            sym: ctx._name.text!
+        }
     }
 
     visitParanexpr(ctx: ParanexprContext) : Expression {
@@ -126,6 +137,7 @@ export class ScalaSlangVisitorInstance
 
 
         return {
+            kind: "lambda",
             params: this.visit(ctx.names()),
             body
         } as LambdaExpr
@@ -133,6 +145,7 @@ export class ScalaSlangVisitorInstance
 
     visitCondexpr(ctx: CondexprContext) : CondExpr {
         return {
+            kind: "cond",
             pred: this.visit(ctx.expr(0)) as Expression,
             conseq: this.visit(ctx.expr(1)) as Expression,
             alt: this.visit(ctx.expr(2)) as Expression
