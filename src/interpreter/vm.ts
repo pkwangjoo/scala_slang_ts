b0e8859c3396cbef79224f9fdffb407933042bb2
wpinrui <77185900+wpinrui@users.noreply.github.com>
2024-04-02 16:12:58 +0800

    Init memoryManager

--- src/interpreter/vm.ts
+++ src/interpreter/vm.ts
@@ -9,6 +9,7 @@ class VirtualMachine {
   private programCounter: number = 0;
   private environment: Environment = { frames: [] };
   private runtimeStack: any[] = [];
+  private mem: MemoryManager = new MemoryManager(10000000, 8);
 
   private globalEnvironment: Environment = { frames: [
     // TODO: Add built-in functions to the global environment
@@ -27,9 +28,24 @@ class VirtualMachine {
     return this.runtimeStack[this.runtimeStack.length - 1];
   }
 
+  private applyUnop(op: string, arg: any): any {
+    if (op === "!") {
+      return !arg;
+    }
+    if (op === "-") {
+      return -arg;
+    }
+    throw new Error("Not implemented");
+  }
+
   execute(instruction: Instruction) {
     if (instruction.kind === "LDC") {
       this.operandStack.push(instruction.val);
+    }
+    if (instruction.kind === "LD") {
+      this.operandStack.push(
+        this.applyUnop(instruction.sym, this.operandStack.pop())
+      );
     }
     throw new Error("Not implemented");
   }
