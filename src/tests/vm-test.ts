db13af7c2e8ec0dc4d2b32d69fd0da84cdb46b6d
Ivan <77185900+wpinrui@users.noreply.github.com>
2024-04-07 21:36:03 +0800

    Update vm-test.ts

--- src/tests/vm-test.ts
+++ src/tests/vm-test.ts
@@ -1,9 +1,11 @@
 export const vm_test_basic = `
 {
   {
-    val a = 4;
-    val b = 5;
-    a + b;
+    val a = 1;
+    val b = 2;
+    val c = a + b;
+    val d = 3 + c;
+    d + 4 + 5;
   }
 }
 `;
