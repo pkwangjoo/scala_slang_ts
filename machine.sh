753e4ae1206015bc4049a1a8f607d486ecf1f4f9
Ivan <77185900+wpinrui@users.noreply.github.com>
2024-04-21 15:18:50 +0800

    Update machine.sh

--- machine.sh
+++ machine.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 FILE_PATH=$1
 USE_TYPE_INFERENCE=$2
+
 if [ -z "$FILE_PATH" ]; then
   read -p "Enter the file path: " FILE_PATH
 fi
-node dist/src/interpreter/main.js %FILE_PATH% %USE_TYPE_INFERENCE%
+
+node dist/src/interpreter/main.js $FILE_PATH $USE_TYPE_INFERENCE
+
 read -p "Press any key to continue..."
